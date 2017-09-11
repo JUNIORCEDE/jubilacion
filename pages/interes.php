c601f8f79acfc07ea96fb1175f09c4565b98773f
PATITO\JUNIORCEDE <juniorcede2@hotmail.com>
2017-09-11 12:50:46 -0500

    interes php

--- pages/interes.php
+++ pages/interes.php
@@ -1,21 +1,53 @@
 <?php
-require_once '../entidades/interes.entidad.php';
-require_once '../models/interes.model.php';
+require_once '../model.php';
 
 // Logica
-$inte = new Interes();
-$model = new InteresModel();
+//$aport = new Aporte();
+//$model = new AporteModel();
+$model = new Model();
+// se obtiene la cedula por medio de la url
+$cedula = (int)(!isset($_GET['cedula'])) ? null : $_GET['cedula'];;
+//si presiona consultar se envia la cedula
+if(isset($_POST['Consultar']))
+{
+    $cedula = $_POST['Cedula'];
+}
+elseif(isset($_POST['Nuevo'])){
+    $cedula = null;
+}
+$compag =(int)(!isset($_GET['pag'])) ? 1 : $_GET['pag'];
+$TotalRegistro = 0;
+$CantidadMostrar = 20;
+$consultavistas ="SELECT * FROM interes ORDER BY interes.ID_INTERES ASC LIMIT ".(($compag-1)*$CantidadMostrar)." , ".$CantidadMostrar;
+if (!is_null($cedula) and !($cedula=="") and ($cedula!=0)){
+    $consulta = $model->Listar("SELECT * FROM interes where CEDULA = ".$cedula);
+    $cont = count($model->result);
+    $TotalRegistro = ceil($cont/$CantidadMostrar);
+    $consultavistas ="SELECT * FROM interes where CEDULA = ".$cedula." ORDER BY interes.ID_INTERES ASC LIMIT ".(($compag-1)*$CantidadMostrar)." , ".$CantidadMostrar;
+}
+else{
+    $model->Listar("SELECT * FROM interes");
+    $cont = count($model->result);
+    $TotalRegistro = ceil($cont/$CantidadMostrar);
+}
+
+if ($compag > $TotalRegistro) {
+    echo '<script type="text/javascript">location.href="?pag=1&cedula='.$cedula.'";</script>';
+}
+
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
-	<head>
-		<title>Datos</title>
+    <head>
+        <title>Datos</title>
         <link href="../style.css" rel="stylesheet" type="text/css" />
         <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
         <script type="text/javascript" href="../bootstrap/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
+        <link rel="stylesheet" href="../bootstrap/css/paginacion.css">
         <meta charset="utf-8">
-	</head>
+    </head>
     <body style="padding:25px;">
     <h4>Tablas restauradas</h4><br>
         <div class="menu" >
@@ -37,6 +69,28 @@ $model = new InteresModel();
         </div>
         <br><br><br>
         <div>
+
+         <div class="pure-g">
+            <div class="pure-u-1-12">
+                <form method="post" class="pure-form pure-form-stacked" style="margin-bottom:30px;">
+                    <table>
+                        <tr>    
+                            <td><label>Cedula: </label></td>
+                            <td></td><td></td>
+                            <td><input type="text" name="Cedula" value="<?php echo $cedula; ?>" /></td>
+                        </tr>
+                    </table>        
+                    <br>
+                    <table>                     
+                    <tr>
+                    <td><button type="submit" name="Consultar" class="pure-button pure-button-primary">Consultar</button></td>
+                    
+                    <td><button type="submit" name="Nuevo" class="pure-button pure-button-primary">Nuevo</button></td>
+                    </tr>
+                    </table>
+                </form>
+                </div>
+                </div>
                 <table class="table table-bordered table-condensed table-hover">
                     <thead>
                         <tr>
@@ -49,18 +103,52 @@ $model = new InteresModel();
                             <th style="text-align:left;">Monto</th>
                         </tr>
                     </thead>
-                    <?php foreach($model->Listar() as $r): ?>
-                        <tr>
-                            <td><?php echo $r->__GET('id_interes'); ?></td>
-                            <td><?php echo $r->__GET('anio'); ?></td>
-                            <td><?php echo $r->__GET('cedula'); ?></td>
-                            <td><?php echo $r->__GET('capital'); ?></td>
-                            <td><?php echo $r->__GET('captota'); ?></td>
-                            <td><?php echo $r->__GET('interes'); ?></td>
-                            <td><?php echo $r->__GET('monto'); ?></td>
-                        </tr>
-                    <?php endforeach; ?>
+                <?php 
+                    $model->Listar($consultavistas);
+                    foreach( $model->result as $r): ?>
+                    <tr>
+                        <td style="text-align:left;padding:.5em 5em"><?php echo $r->ID_INTERES; ?></td>
+                        <td style="text-align:left;padding:.5em 5em"><?php echo $r->ANIO; ?></td>
+                        <td style="text-align:left;padding:.5em 5em"><?php echo $r->CEDULA; ?></td> 
+                        <td style="text-align:left;padding:.5em 5em"><?php echo $r->CAPITAL; ?></td> 
+                        <td style="text-align:left;padding:.5em 5em"><?php echo $r->CAPTOTA; ?></td>
+                        <td style="text-align:left;padding:.5em 5em"><?php echo $r->INTERES; ?></td>
+                        <td style="text-align:left;padding:.5em 5em"><?php echo $r->MONTO; ?></td>
+                    </tr>
+                <?php endforeach; ?>
                 </table>
+                <?php
+                    if ($TotalRegistro>1) {
+                    /*Sector de Paginacion */
+                    //Operacion matematica para botón siguiente y atrás 
+                    $IncrimentNum =(($compag +1)<=$TotalRegistro)?($compag +1):1;
+                    $DecrementNum =(($compag -1))<1?1:($compag -1);
+                ?>
+                    <ul><li><a href=<?php echo '?pag='.$DecrementNum."&cedula=".$cedula;?> >◀</a></li>
+                <?php
+                   //Se resta y suma con el numero de pag actual con el cantidad de 
+                    //números  a mostrar
+                     $Desde=$compag-(ceil($CantidadMostrar/2)-1);
+                     $Hasta=$compag+(ceil($CantidadMostrar/2)-1);
+                     //Se valida
+                     $Desde=($Desde<1)?1: $Desde;
+                     $Hasta=($Hasta<$CantidadMostrar)?$CantidadMostrar:$Hasta;
+                     //Se muestra los números de paginas
+                     for($i=$Desde; $i<=$Hasta;$i++){
+                        //Se valida la paginacion total
+                        //de registros
+                        if($i<=$TotalRegistro){
+                            //Validamos la pag activo
+                          if($i==$compag){
+                           echo "<li class=\"active pag\"><a href=\"?pag=".$i."&cedula=".$cedula."\">".$i."</a></li>";
+                          }else {
+                            echo "<li class=\"pag\"><a href=\"?pag=".$i."&cedula=".$cedula."\">".$i."</a></li>";
+                          }             
+                        }
+                     }
+                    echo "<li class=\"btnp pag\"><a href=\"?pag=".$IncrimentNum."&cedula=".$cedula."\">▶</a></li></ul>";
+                     } 
+                 ?>
         </div>
 
     </body>
